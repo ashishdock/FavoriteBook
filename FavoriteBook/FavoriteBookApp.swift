30c53ac0b136550463c2d7c57ec5ec37aed88234
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-26 15:53:24 +0530

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Ashish Sharma on 12/26/2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
